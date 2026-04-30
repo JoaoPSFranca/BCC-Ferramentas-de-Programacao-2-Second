67af13a8488e79ee3e67d00794063be3eaa9ccca
JoaoPSFranca <francasjoaopedro@gmail.com>
2026-04-30 08:51:46 -0300

    feat: continue primeng project (filter)

--- day15_UI/primeng/src/app/pages/vendas/vendas.ts
+++ day15_UI/primeng/src/app/pages/vendas/vendas.ts
@@ -2,18 +2,27 @@ import { Component, inject } from '@angular/core';
 import { VendaService } from '../../services/venda.service';
 import { AsyncPipe, DatePipe } from '@angular/common';
 import { TableModule } from 'primeng/table';
+import { IconField } from 'primeng/iconfield';
+import { InputIcon } from 'primeng/inputicon';
+import { InputText } from 'primeng/inputtext';
+import { Venda } from '../../models/venda.model';
 
 @Component({
   selector: 'app-vendas',
-  imports: [AsyncPipe, TableModule, DatePipe],
+  imports: [AsyncPipe, TableModule, DatePipe, IconField, InputIcon, InputText],
   templateUrl: './vendas.html',
   styleUrl: './vendas.scss',
 })
 export class Vendas {
   private vendaService = inject(VendaService);
   protected vendas$ = this.vendaService.getAll();
+  protected selectedVenda!: Venda;
 
   constructor() {
     console.log(this.vendas$);
+  }
+
+  protected test() {
+    console.log(this.selectedVenda);
   }
 }
